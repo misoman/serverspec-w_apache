fb89c09713876ebe33f8e3195158d67e525d60ad
Full Of Lilies <fulloflilies@gmail.com>
2015-11-20 15:10:22 -0500

    change docroot in vhosts_spec

--- serverspec/localhost/vhosts_spec.rb
+++ serverspec/localhost/vhosts_spec.rb
@@ -2,14 +2,8 @@ require 'spec_helper'
 
 describe 'w_apache::vhosts' do
 
-	describe file('/websites') do
-	  it { should be_directory }
-	  it { should be_owned_by 'www-data' }
-	  it { should be_grouped_into 'www-data' }
-	end
-
-	%w( examplewebsite.com examplewebsite.com/admin ).each do |docroot|
-		describe file("/websites/#{docroot}") do
+	%w( /websites/examplewebsite.com/www /websites/examplewebsite.com/admin ).each do |docroot|
+		describe file("#{docroot}") do
 		  it { should be_directory }
 		  it { should be_owned_by 'www-data' }
 		  it { should be_grouped_into 'www-data' }
@@ -17,14 +11,14 @@ describe 'w_apache::vhosts' do
 	end
 
 	[
-		{'main_domain'=> 'examplewebsite.com', 'docroot'=> 'examplewebsite.com', 'aliases'=> ['www.examplewebsite.com'] },
-		{'main_domain'=> 'admin.examplewebsite.com', 'docroot'=> 'examplewebsite.com/admin', 'aliases'=> ['admin.examplewebsite.com'] }
+		{'main_domain'=> 'examplewebsite.com', 'docroot'=> '/websites/examplewebsite.com/www', 'aliases'=> ['www.examplewebsite.com'] },
+		{'main_domain'=> 'admin.examplewebsite.com', 'docroot'=> '/websites/examplewebsite.com/admin', 'aliases'=> ['admin.examplewebsite.com'] }
 	].each do |vhost|
 
 	 	describe file("/etc/apache2/sites-available/#{vhost['main_domain']}.conf") do
 	     it { should be_file }
-	     it { should contain('AllowOverride All').from("<Directory /websites/#{vhost['docroot']}>").to('</Directory>') }
-	     it { should contain("ServerName #{vhost['main_domain']}").before("DocumentRoot /websites/#{vhost['docroot']}") }
+	     it { should contain('AllowOverride All').from("<Directory #{vhost['docroot']}>").to('</Directory>') }
+	     it { should contain("ServerName #{vhost['main_domain']}").before("DocumentRoot #{vhost['docroot']}") }
 	     it { should contain("ServerAlias #{vhost['aliases']}").after("ServerName #{vhost['main_domain']}") }
 	     it { should contain('DirectoryIndex index.html index.htm index.php') }
 	   end
