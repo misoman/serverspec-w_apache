1f009daeea7df50b0712b217c1ca29df72836d81
Full Of Lilies <fulloflilies@gmail.com>
2016-01-04 13:48:18 -0500

    Revert "Allow symple syntax"

--- serverspec/localhost/varnish_integration_spec.rb
+++ serverspec/localhost/varnish_integration_spec.rb
@@ -2,13 +2,15 @@ require 'spec_helper'
 
 describe 'w_apache::varnish_integration' do
 
-  describe file('/var/www/html/ping.php') do
-    it { should be_file }
-    it { should contain '<html><body>website is healthy</body></html>' }
-  end
-
-  describe host('0varnish.example.com') do
-    it { should be_resolvable.by('hosts') }
-    its(:ipaddress) { should eq '172.31.2.12' }
-  end
+	describe file('/var/www/html/ping.php') do
+	  it { should be_file }
+	  it { should contain '<html><body>website is healthy</body></html>' }
+	end
+	
+  # commented as it is not tested in different IP range
+	#describe host('0varnish.examplewebsite.com') do
+  #  it { should be_resolvable.by('hosts') }
+  #  its(:ipaddress) { should eq '172.31.2.12' }
+  #end
+  
 end
--- serverspec/localhost/vhosts_spec.rb
+++ serverspec/localhost/vhosts_spec.rb
@@ -2,29 +2,31 @@ require 'spec_helper'
 
 describe 'w_apache::vhosts' do
 
-  [
-    { 'main_domain' => 'example.com', 'aliases' => ['www.example.com', 'ex.com'], 'docroot' => '/websites/example.com/www'  },
-    { 'main_domain' => 'example2.com',                                            'docroot' => '/websites/example2.com/sub' },
-    { 'main_domain' => 'example3.com',                                            'docroot' => '/websites/example3.com/sub' },
-    { 'main_domain' => 'docroot-only-vhost.com',                                  'docroot' => '/websites/dov'              }
-  ].each do |vhost|
+	%w( /websites/examplewebsite.com/www /websites/examplewebsite.com/admin ).each do |docroot|
+		describe file("#{docroot}") do
+		  it { should be_directory }
+		  it { should be_owned_by 'www-data' }
+		  it { should be_grouped_into 'www-data' }
+		end
+	end
 
-    describe file("#{vhost['docroot']}") do
-      it { should be_directory }
-      it { should be_owned_by 'www-data' }
-      it { should be_grouped_into 'www-data' }
-    end
+	[
+		{'main_domain'=> 'examplewebsite.com', 'docroot'=> '/websites/examplewebsite.com/www', 'aliases'=> ['www.examplewebsite.com'] },
+		{'main_domain'=> 'admin.examplewebsite.com', 'docroot'=> '/websites/examplewebsite.com/admin', 'aliases'=> ['admin.examplewebsite.com'] }
+	].each do |vhost|
 
-    describe file("/etc/apache2/sites-available/#{vhost['main_domain']}.conf") do
-      it { should be_file }
-      it { should contain('AllowOverride All').from("<Directory #{vhost['docroot']}>").to('</Directory>') }
-      it { should contain("ServerName #{vhost['main_domain']}").before("DocumentRoot #{vhost['docroot']}") }
-      it { should contain("ServerAlias #{vhost['aliases']}").after("ServerName #{vhost['main_domain']}") } if vhost.has_key?('aliases')
-      it { should contain('DirectoryIndex index.html index.htm index.php') }
-    end
+	 	describe file("/etc/apache2/sites-available/#{vhost['main_domain']}.conf") do
+	     it { should be_file }
+	     it { should contain('AllowOverride All').from("<Directory #{vhost['docroot']}>").to('</Directory>') }
+	     it { should contain("ServerName #{vhost['main_domain']}").before("DocumentRoot #{vhost['docroot']}") }
+	     it { should contain("ServerAlias #{vhost['aliases']}").after("ServerName #{vhost['main_domain']}") }
+	     it { should contain('DirectoryIndex index.html index.htm index.php') }
+	   end
 
-    describe file("/etc/apache2/sites-enabled/#{vhost['main_domain']}.conf") do
-      it { should be_linked_to "../sites-available/#{vhost['main_domain']}.conf" }
-    end
-  end
+	   describe file("/etc/apache2/sites-enabled/#{vhost['main_domain']}.conf") do
+	     it { should be_linked_to "../sites-available/#{vhost['main_domain']}.conf" }
+	   end
+
+	 end
+
 end
