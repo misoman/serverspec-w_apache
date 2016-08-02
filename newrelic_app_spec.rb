e19398a3530f2f75bf0f571264fc42536e1e0da5
Joel Handwell <joelhandwell@gmail.com>
2016-08-01 21:51:40 -0400

    Ensure deleting duplicated newrelic app config

--- newrelic_app_spec.rb
+++ newrelic_app_spec.rb
@@ -6,7 +6,10 @@ RSpec.shared_examples 'w_apache::newrelic_app' do
     it { should be_executable }
   end
 
-  describe file('/etc/php/#{php_minor_version}/fpm/conf.d/newrelic.ini') do
-    it { should_not exist }
+  %w(fpm cli).each do |execution_type|
+
+    describe file("/etc/php/#{php_minor_version}/#{execution_type}/conf.d/newrelic.ini") do
+      it { should_not exist }
+    end
   end
 end
