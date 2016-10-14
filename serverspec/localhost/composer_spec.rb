542d02074924ebac557111c3df636fe8aa7554dc
Miso Podolak <miso.podolak@gmail.com>
2016-10-14 13:07:02 +0200

    add test for composer home folder and composer -h command as www-data user

--- serverspec/localhost/composer_spec.rb
+++ serverspec/localhost/composer_spec.rb
@@ -5,9 +5,21 @@ describe 'w_apache::composer' do
   describe file('/usr/local/bin/composer') do
     it { should be_file }
   end
+
+  describe file('/var/www/composer') do
+    it { should be_directory }
+    it { should be_owned_by 'www-data' }
+    it { should be_grouped_into 'www-data' }
+  end
   
   describe command('composer') do
+ 	let(:sudo_options) { '-u www-data -i' }
     its(:stdout) { should match /Composer version/ }
+    its(:exit_status) { should eq 0 }
+  end
+
+  describe command('composer -h') do
+    let(:sudo_options) { '-u www-data -i' }
     its(:exit_status) { should eq 0 }
   end
   
